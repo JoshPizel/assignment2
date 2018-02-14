23290f1b29e8ccc16915a73efa0bb1440437afeb
Josh Pizel <joshpizel@AD.DOE.CARLETON.CA>
2018-02-14 13:29:51 -0500

    initial commit

---
+++ part1.m
@@ -0,0 +1,19 @@
+clearvars
+clearvars -GLOBAL
+close all 
+
+
+global C
+
+C.q_0 = 1.60217653e-19;             % electron charge
+    C.hb = 1.054571596e-34;             % Dirac constant
+    C.h = C.hb * 2 * pi;                % Planck constant
+    C.m_0 = 9.10938215e-31;             % electron mass
+    C.kb = 1.3806504e-23;               % Boltzmann constant
+    C.eps_0 = 8.854187817e-12;          % vacuum permittivity
+    C.mu_0 = 1.2566370614e-6;           % vacuum permeability
+    C.c = 299792458;                    % speed of light
+    C.g = 9.80665;
+    
+    
+L
