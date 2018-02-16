aa73467a49bbe6870fbc442ef40752d8a739c61f
Josh Pizel <joshpizel@AD.DOE.CARLETON.CA>
2018-02-16 13:25:36 -0500

    further work

--- part1.m
+++ part1.m
@@ -19,22 +19,22 @@ C.q_0 = 1.60217653e-19;             % electron charge
 L = 75;
 W = 50;
 
-G = zeros(L*W,L*W);
+G = sparse(L*W,L*W);
 
 for i =1:1:L
     for j =1:1:W
         n = j+(i-1)*W;
-        
         nxm = j+(i-2)*W;
         nxp = j+i*W;
         nyp = j+1+ (i-1)*W;
         nym = j-1+ (i-1)*W;
+        
         if(i==1)
             G(n,:) = 0;
             G(n,n) = 1;
         elseif(i==L)
             G(n,:) = 0;
-            G(n,n) = 1;
+            G(n,n) = 0;
         elseif(j==1)
             G(n,:) = 0;
             G(n,n) = 1;
@@ -42,16 +42,35 @@ for i =1:1:L
             G(n,:) = 0;
             G(n,n) = 1;
         else          
-            G(n,n) = -4;
+            G(n,:)=0;
             G(n,nxm) = 1; 
             G(n,nxp) = 1;
             G(n,nym) = 1;
             G(n,nyp) = 1;
+            G(n,n) = -(G(n,nxm)+G(n,nxp)+G(n,nym)+G(n,nyp));
         end
     end
 end
 
-[V,D] = eigs(G,9,'SM');
+[V,D] = eigs(G);
 
+%remap
+Vs = zeros(L,W);
+for i =1:1:L
+    for j =1:1:W
+        n=j+(i-1)*W;
+        
+        Vs(i,j) =V(n,1);
+        %+V(n,2)+V(n,3)+V(n,4)+V(n,5)+V(n,6)
+    end
+end
 
+%http://www.southampton.ac.uk/~feeg6002/lecturenotes/feeg6002_numerical_methods05.pdf
+
+F = zeros(L,W);
+F(1,:) =1;
+
+Vs2 =F./Vs;
+
+surf(Vs2);
 
